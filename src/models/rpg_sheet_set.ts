2f8b1ec088b3b4f97f01316fb9b98e0ba4de2788
Jhonny A. Soares <jhonny.mg35@gmail.com>
2026-05-22 23:33:12 -0300

    removendo a pasta generated

---
+++ src/models/rpg_sheet_set.ts
@@ -0,0 +1,80 @@
+import prisma from "../prisma";
+
+import { RpgSheetAction } from "../generated/client";
+
+type UpdateOrCreateProps = {
+  name: string;
+  guildId: string;
+  cell: string;
+  dice?: string;
+  action?: RpgSheetAction;
+};
+
+export async function updateOrCreate({
+  name,
+  guildId,
+  cell,
+  dice,
+  action,
+}: UpdateOrCreateProps) {
+  try {
+    return await prisma.rpgSheetSet.upsert({
+      where: {
+        name_guildId: {
+          name,
+          guildId,
+        },
+      },
+
+      update: {
+        cell,
+        dice,
+        action,
+      },
+
+      create: {
+        name,
+        guildId,
+        cell,
+        dice,
+        action,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    throw new Error("Erro ao salvar sheet set.");
+  }
+}
+
+export async function getOnly(name: string, guildId: string) {
+  try {
+    return await prisma.rpgSheetSet.findUnique({
+      where: {
+        name_guildId: {
+          name,
+          guildId,
+        },
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    throw new Error("Erro ao buscar sheet set.");
+  }
+}
+
+export async function remove(name: string, guildId: string) {
+  try {
+    return await prisma.rpgSheetSet.delete({
+      where: {
+        name_guildId: {
+          name,
+          guildId,
+        },
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    throw new Error("Erro ao deletar sheet set.");
+  }
+}
